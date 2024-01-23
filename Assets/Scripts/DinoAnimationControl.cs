f463530792a0be01e0bf07c1aa401f63c08c1040
Vincent Juré <vincent.jure@gmail.com>
2024-01-23 16:59:29 +0100

    Add dino's + Dino circuit & Sounds

---
+++ Assets/Scripts/DinoAnimationControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoAnimation : MonoBehaviour
+{
+
+    public string dinoAnimtion;
+    public bool isDanger;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator != null)
+        {
+            animator.Play(dinoAnimtion);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isDanger)
+        {
+            animator.Play("TailAttack");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isDanger)
+        {
+            animator.Play("Sleep");
+        }
+    }
+}
