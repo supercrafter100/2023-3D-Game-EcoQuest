9349ebdd53b5c22e4bdd22561e3bc213390f2b83
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2024-01-24 18:40:16 +0100

    Add hub teleporter to tree planter scene and remove lightmap (#13)

--- Assets/Scripts/treePlanter/GameManager.cs
+++ Assets/Scripts/treePlanter/GameManager.cs
@@ -9,16 +9,19 @@ namespace treePlanter
     {
         public TMP_Text treesTextElement;
         public TMP_Text seedsTextElement;
+        public TMP_Text winnerTextElement;
 
         public int seeds = 0;
         public int trees = 0;
 
         public int treesGoal = 20;
     
+        public bool isFinished => trees >= treesGoal;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            winnerTextElement.SetText("");
         }
 
         // Update is called once per frame
@@ -27,6 +30,8 @@ namespace treePlanter
             // Update ui elements
             treesTextElement.SetText("Bomen geplant: " + trees + " / " + treesGoal);
             seedsTextElement.SetText("Zaden: " + seeds);
+            
+            if (isFinished) winnerTextElement.SetText("Je hebt genoeg bomen geplant! Keer terug naar de hub via de teleporter bij je spawnlocatie");
         }
     }    
 }
